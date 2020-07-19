375a053822e0d0ab3d464171b02ad0a08837f83d
Mushtaq A Halepoto <mahalepoto@outlook.com>
2020-07-19 20:48:38 +0100

    ex:18 adding object properties done

--- module-1/18-adding-object-properties/index.js
+++ module-1/18-adding-object-properties/index.js
@@ -9,12 +9,16 @@ const user = {};
  * 
  * add property "firstName" to "user" and assign any name 
  */
+user.firstName = "John";
+console.log(user);
 
 /**
  * Exercise 2
  * 
  * add property "lastName" to "user" and assign any last name 
  */
+user.lastName = "Halepoto";
+console.log(user);
 
 /**
  * Exercise 3
@@ -22,3 +26,8 @@ const user = {};
  * 2 params, first - property name and second - property value
  * and set a new property for 'user' object
  */
+function addANewProperty(key, value) {
+    user[key] = value;
+    return user;
+}
+console.log(addANewProperty('age', 25));
