9fbb04afb3a2e91725acfbb07962769a0c0d28d9
Mushtaq A Halepoto <mahalepoto@outlook.com>
2020-07-20 18:54:35 +0100

    ex:20 for loops done.

--- module-1/20-for-loop-exercise/index.js
+++ module-1/20-for-loop-exercise/index.js
@@ -1,4 +1,4 @@
-const myName = "John"; // SET YOUR NAME
+const myName = "Mushtaq"; // SET YOUR NAME
 
 const visitors = ["Alex", "Mike", "Sam"]; // Add at least 5 names
 
@@ -9,6 +9,12 @@ const visitors = ["Alex", "Mike", "Sam"]; // Add at least 5 names
  * will greet all people from "visitors" for each person you should
  * console.log "Hi {name}, my name is {myName}!"
  */
+function greetVisitors() {
+    for (i = 0; i < visitors.length; i++) {
+        console.log('Hi ' + visitors[i] + ', my name is ' + myName);
+    }
+}
+greetVisitors();
 
 // ========================
 const numbers = [2, 123, 13, 1, 6, 84, 23, 12, 45]; // DON'T TOUCH THIS LINE :)
@@ -19,6 +25,14 @@ const numbers = [2, 123, 13, 1, 6, 84, 23, 12, 45]; // DON'T TOUCH THIS LINE :)
  * given item in {numbers}.
  * If the item is not present, return a string "Item not found"
  */
+function getElementIndex(checkNumber) {
+    for (var i = 0; i < numbers.length; i++) {
+        if (checkNumber === numbers[i]) {
+            return i;
+        } 
+    } return "Item not found";
+}
+console.log(getElementIndex(200));
 
 // ========================
 
@@ -27,7 +41,15 @@ const numbers = [2, 123, 13, 1, 6, 84, 23, 12, 45]; // DON'T TOUCH THIS LINE :)
  * Create a function {total} that will return sum of all numbers
  * from {numbers} array
  */
-
+function total() {
+    let sum = 0;
+    for (i = 0; i < numbers.length; i++){
+        // console.log(numbers[i]);
+        sum += numbers[i];
+        // console.log(sum);
+    } return sum;
+}
+console.log(total());
 // ========================
 
 const numbersForIndexes = [6, 2, 16, 23, 84, 21, 9, 3]; // DON'T TOUCH THIS LINE :)
@@ -37,6 +59,17 @@ const numbersForIndexes = [6, 2, 16, 23, 84, 21, 9, 3]; // DON'T TOUCH THIS LINE
  * Create a function {addIndex} that takes {numbersForIndexes} array,
  * and returns a new array which includes each number + it's index
  */
+function addIndex() {
+    let newArray = [];
+    for (let i = 0; i < numbersForIndexes.length; i++) {
+        newArray.push(numbersForIndexes[i] + i);
+        // console.log(numbersForIndexes[i]);
+        // console.log(i);
+            
+    } return newArray;
+}
+console.log(addIndex());
+
 
 const forLowestNumber = [5, 234, 96, 34, -34, 0, 23]; // DON'T TOUCH THIS LINE :)
 /**
@@ -44,3 +77,18 @@ const forLowestNumber = [5, 234, 96, 34, -34, 0, 23]; // DON'T TOUCH THIS LINE :
  *  create function {lowestNumber} which will return the lowest integer
  * from {forLowestNumber}
  */
+
+ function lowestNumber() {
+     let smallestNumber = 0;
+     for (i = 0; i < forLowestNumber.length; i++) {
+         let smallerNumber = forLowestNumber[i]
+         if (smallestNumber < smallerNumber) {
+             smallestNumber = smallestNumber;
+            //  console.log(smallestNumber);            
+         } else {
+             smallestNumber = smallerNumber;
+            //  console.log(smallestNumber);
+         }
+     } return smallestNumber;
+ }
+ console.log(lowestNumber());
