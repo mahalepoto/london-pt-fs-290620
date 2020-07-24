b7f4049f1017becb81dd9e7b36922e2a781d6262
Mushtaq A Halepoto <mahalepoto@outlook.com>
2020-07-24 14:33:17 +0100

    ex27 arrow function, done

--- module-1/27-arrow-functions-exercise/index.js
+++ module-1/27-arrow-functions-exercise/index.js
@@ -8,6 +8,9 @@
  * create an arrow function {isEqual} which takes 2 numbers as params
  * and return true if they are equal or false if not
  */
+let isEqual = (num1, num2) => (num1 === num2) ? true : false;
+// console.log(isEqual(2, '2')); testing 
+
 
 /**
  * Exercise 2
@@ -15,6 +18,8 @@
  * create an arrow function {isLessOrEqual0} which takes number as a param
  * and return true if it equals or less than 0 or false if not
  */
+let isLessOrEqual0 = (number) => (number <= 0) ? true : false;
+// console.log(isLessOrEqual0(-11)); testing
 
 /**
  * Exercise 3
@@ -26,6 +31,8 @@
  * Don't forget to set default values for your params, 0 and 0
  * so you will not break your function if you forget to pass params
  */
+let timeToSeconds = (hours =0, minutes =0) => ((hours * 60) * 60) + (minutes * 60);
+// console.log(timeToSeconds()); testing
 
 /**
  * Exercise 4
@@ -35,6 +42,9 @@
  * pay. If probability * prize is more than
  * pay then return true otherwise return false
  */
+let isGameWorthTheCandle = (probability, prize, pay) => (probability * prize > pay) ? true : false;
+// console.log(isGameWorthTheCandle(0.2, 3, 10));
+
 
 /**
  * Exercise 5
@@ -43,3 +53,12 @@
  * as parameters and returns the string which is longest. If the strings are
  * equal in length return the string "Equal!"
  */
+let longerString = (str1, str2) => {
+    if (str1.length < str2.length) {
+        return str2;
+    } else if (str1.length === str2.length) {
+        return 'Equal!'
+    }
+    return str1
+}
+// console.log(longerString('Lion', 'Dog')); testing
